4a6a070edaff3ee3f9f89566d4248d619b7dd695
yanshouwang <yanshouwang@outlook.com>
2021-02-17 16:40:03 +0800

    reorder code

--- ios/Classes/SwiftCameraXPlugin.swift
+++ ios/Classes/SwiftCameraXPlugin.swift
@@ -78,9 +78,9 @@ public class SwiftCameraXPlugin: NSObject, FlutterPlugin, FlutterStreamHandler, 
                 break
             }
             analyzing = true
+            let scanner = BarcodeScanner.barcodeScanner()
             let buffer = CMSampleBufferGetImageBuffer(sampleBuffer)
             let image = VisionImage(image: buffer!.image)
-            let scanner = BarcodeScanner.barcodeScanner()
             scanner.process(image) { [self] barcodes, error in
                 if error == nil && barcodes != nil {
                     for barcode in barcodes! {
