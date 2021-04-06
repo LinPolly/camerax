8b138a99a0dc57358c83396bf36692fcadd862b4
yanshouwang <yanshouwang@outlook.com>
2021-04-06 21:51:15 +0800

    redesign

--- android/src/main/kotlin/dev/yanshouwang/camerax/Camera.kt
+++ android/src/main/kotlin/dev/yanshouwang/camerax/Camera.kt
@@ -2,7 +2,11 @@ package dev.yanshouwang.camerax
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.Context
+import android.hardware.camera2.*
+import android.os.Handler
 import android.util.Log
+import android.util.Size
 import android.view.Surface
 import androidx.annotation.IntDef
 import androidx.camera.core.*
@@ -11,23 +15,104 @@ import androidx.core.content.ContextCompat
 import androidx.core.util.Consumer
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import com.google.mlkit.vision.barcode.BarcodeScanning
 import com.google.mlkit.vision.common.InputImage
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.view.TextureRegistry
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.lang.RuntimeException
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
 
-@androidx.camera.core.ExperimentalGetImage
-class NativeCamera(private val activity: Activity, private val textureRegistry: TextureRegistry) {
+class Camera2(@LensFacing val facing: Int, private val activity: Activity, private val textureRegistry: TextureRegistry) {
+    private val cameraManager = activity.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+    private val cameraId: String
+    private val previewSize: Size
+
     private var analyzeMode = AnalyzeMode.NONE
 
-    private lateinit var cameraProvider: ProcessCameraProvider
-    private lateinit var camera: Camera
+    private lateinit var camera: CameraDevice
     private lateinit var textureEntry: TextureRegistry.SurfaceTextureEntry
+    private lateinit var session: CameraCaptureSession
+
+    private lateinit var cameraProvider: ProcessCameraProvider
+
+    private val lifecycleScope get() = (activity as LifecycleOwner).lifecycleScope
+
+    init {
+        cameraManager.cameraIdList
+                .filter { cameraId ->
+                    val characteristics = cameraManager.getCameraCharacteristics(cameraId)
+                    characteristics[CameraCharacteristics.LENS_FACING] == facing
+                }
+                .forEach { cameraId -> Log.d(TAG, "camera id for $facing: $cameraId") }
+        cameraId = cameraManager.cameraIdList
+                .first { cameraId ->
+                    val characteristics = cameraManager.getCameraCharacteristics(cameraId)
+                    characteristics[CameraCharacteristics.LENS_FACING] == facing
+                }
+        previewSize = computeBestPerviewSize()
+    }
+
+    fun start() = lifecycleScope.launch(Dispatchers.Main) {
+        camera = openCamera()
+        textureEntry = textureRegistry.createSurfaceTexture()
+        val texture = textureEntry.surfaceTexture()
+        texture.setDefaultBufferSize(previewSize.getWidth(), previewSize.getHeight());
+        val surface = Surface(texture)
+        val targets = listOf(surface)
+        session = createCaptureSession(targets)
+    }
+
+    private suspend fun openCamera(handler: Handler? = null) = suspendCoroutine<CameraDevice> { continuation ->
+        cameraManager.openCamera(cameraId, object : CameraDevice.StateCallback() {
+            override fun onOpened(camera: CameraDevice) = continuation.resume(camera)
+
+            override fun onClosed(camera: CameraDevice) {
+                super.onClosed(camera)
+            }
+
+            override fun onDisconnected(camera: CameraDevice) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onError(camera: CameraDevice, error: Int) {
+                val message = when (error) {
+                    ERROR_CAMERA_IN_USE -> "Camera in use"
+                    ERROR_MAX_CAMERAS_IN_USE -> "Maximum cameras in use"
+                    ERROR_CAMERA_DISABLED -> "Device policy"
+                    ERROR_CAMERA_DEVICE -> "Fatal (device)"
+                    ERROR_CAMERA_SERVICE -> "Fatal (service)"
+                    else -> "Unknown"
+                }
+                val exception = RuntimeException("Camera $cameraId error: ($error) $message")
+                continuation.resumeWithException(exception)
+            }
+
+        }, handler)
+    }
+
+    private suspend fun createCaptureSession(targets: List<Surface>, handler: Handler? = null) = suspendCoroutine<CameraCaptureSession> { continuation ->
+        camera.createCaptureSession(targets, object : CameraCaptureSession.StateCallback() {
+            override fun onConfigured(session: CameraCaptureSession) = continuation.resume(session)
+
+            override fun onConfigureFailed(session: CameraCaptureSession) {
+                val exception = RuntimeException("Camera $cameraId session configuration failed")
+                Log.e(TAG, exception.message, exception)
+                continuation.resumeWithException(exception)
+            }
+
+        }, handler)
+    }
 
     fun start(call: MethodCall, result: MethodChannel.Result, messenger: (Any) -> Unit?) {
-        val future = ProcessCameraProvider.getInstance(activity)
         val executor = ContextCompat.getMainExecutor(activity)
+
+        val future = ProcessCameraProvider.getInstance(activity)
         future.addListener(Runnable {
             cameraProvider = future.get()
             textureEntry = textureRegistry.createSurfaceTexture()
@@ -109,11 +194,21 @@ class NativeCamera(private val activity: Activity, private val textureRegistry: 
 }
 
 @IntDef(AnalyzeMode.NONE, AnalyzeMode.BARCODE)
-@Target(AnnotationTarget.FIELD)
 @Retention(AnnotationRetention.SOURCE)
+@Target(AnnotationTarget.FIELD)
 annotation class AnalyzeMode {
     companion object {
         const val NONE = 0
         const val BARCODE = 1
+    }
+}
+
+@IntDef(LensFacing.FRONT, LensFacing.BACK, LensFacing.EXTERNAL)
+@Retention(AnnotationRetention.SOURCE)
+annotation class LensFacing {
+    companion object {
+        const val FRONT = 0
+        const val BACK = 1
+        const val EXTERNAL = 2
     }
 }
